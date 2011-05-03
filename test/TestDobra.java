81b9669f19e33be0b4f84ddcf8335884c7159381
Thiago Garcia <thiagogds14@gmail.com>
2011-05-03 09:28:29 -0300

    Inicializa a base do projeto

---
+++ test/TestDobra.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author thiago
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({})
+public class TestDobra extends Assert {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
